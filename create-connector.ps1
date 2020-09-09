74a1492e0f6ea1141c770ea314133bf8747eba79
Suat KÖSE <suadev@gmail.com>
2020-09-09 11:55:33 +0300

    kafa connect post config

---
+++ create-connector.ps1
@@ -0,0 +1,32 @@
+$headers = @{
+    'Content-Type' = 'application/json'
+}
+
+$body = @"
+{
+    "name": "product-connector",
+    "config": {
+        "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+        "tasks.max": "1",
+        "database.hostname": "postgres",
+        "database.port": "5432",
+        "database.user": "admin",
+        "database.password": "admin",
+        "database.dbname": "product",
+        "database.server.name": "dbserver1",
+        "database.whitelist": "product",
+        "database.history.kafka.bootstrap.servers": "kafka:9092",
+        "database.history.kafka.topic": "schema-changes.product"
+    }
+}
+"@
+
+$params = @{
+    Uri         = 'http://localhost:8083'
+    Headers     = $headers
+    Method      = 'POST'
+    Body        = $body
+    ContentType = 'application/json'
+}
+
+Invoke-RestMethod @params
